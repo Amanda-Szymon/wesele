ba25aaa85e0df93b7faa0c6ce6d54658dfc7f7ac
Piotr Chmura <piotr.chmura17@gmail.com>
2025-07-12 19:31:14 +0200

    Small fix

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-const ACCESS_TOKEN = 'sl.u.AF2g7z5iVuB42KegiI6AMTYWqXNMr4inldFv3zLuohOnRO-p12SCJ64oEECdW1ihmherjO9POTTAeB5epx4pJOqVaXosKjIZMmO1kpX3vsCOvKC1blV2zwtJ7YzDIJ4Cg735QCk6Vz_Bhyyd_bDuyrUSfnI4QPWmAzb8VvWVrXAudeTyKi7ar4tBKwDmN_bWrYtqQo63l15N6Z7r9tUrIf-Lk_KPQXKRtEqc725yTbMcE3XvxqELZ8vuV3oy9GQMyyKcA-PrYb--BC1MkwOKqSbRRSqNuthT3leI77VIUu_q1Q8OJ6KAnTkp5eakL_43PVDEhM4HsMeQihzZbnBzMGdAe_dHrzob8spIe8u2X2-YKJypGretKrz3dAqFy39zqRRxqg3mKl2M0FVW5DuMVjkS_XZOo6PUd3mvjp52NEOwkuT6SGAfOeM_mWqcoAO4y8-YDvExkXbkA7DCdR3Vu16HuEjtv7K1yXQyzvAjcz-proQmhRAR26pXJ0mFrq7Mq81GNsfUfi3DgI-WnmfaWKNOf4G6BNM6lYUiU-vcUA_MUgHsLLgp7lv2fror20nD-vohG31jiFGMAoGcH_5lwWaXBa2lkv9WZC2M6OrxAjpucbjIjRxzqwbXawfpIGZlNu0mFbDw8wZI9GJvtV1EMNsV8b-jWlt-U6MTwYwoPu4OOA6cMgYdfNtfPTL3mYZdR9mQPwfZjb8dN_wwK_NkUY79e1tgKrKJDn77qN6avVe-8HFtleQ05043e_lit20vOEbeY6USxiVBQwYLyPZucoD4lSLy22QE6yV6Dq1LGidEEXFODYd8q0fnDNLXTx8CkEfUlkWUbJ4K4QE7mN3bJcce0uVdeIBd5BFL4OFlRo1xW6MONv1UTTRMAMEzDUdmbszSfZMqg2AEDFFMn30WuQmMYSfH2l05BuD4X9nvO5qW8p4r-G2nJ7q9KVVLy82_SpI4JeN5wSAj7M7mvsvPfi_KYEGzhUHlFANHcxP7JtOfjwbcF3SE758LPZT97rsvffOfOPPLak4rXuZuGje-JC-prA4Eekm8fRuVJktHAAeXbdGGudUxrejZVJAVY-6XZ3K_w1C1gD7cZt_pQwQPj0__BbBz1xftpLt6Vc9Ln5xzFvB8XWjlQNzC1Z6c6wuJCvK-nj9YkTLkuR7tD7604KojaFRTPF3XVHnAcYdJKoLK-sV5Wa-O-l0yfMG6U5DuB6_Cc_puHsB8VOAIGSv26HQv2WUv6hq1mlIPkupSq9-siuhQTsCpuCHt4CkMqIpmOSGOcAu7tGgXdmW-rFvy392bsa_7UNZCKQu7Bn_CmSjQ5E3zFhxGHJwPepdMVk6jp8R26WekOf8iy6NCOJUyxY5DZIMh3pIOvPVWBDrQ6O188EVi1H_GOqACQ5KCAm8nVheR4EyQg6jeQhZwRJT3ghFfGnjPcrjGWr7W3rQGh_j0LQ';
-  "sl.u.AF0WID0a5FBzaWniH2lnEEVjDl3u-FkB4ghGLHkYq50BMZKdH3cwFgtSgnm-cjsrB_uH3lFkdgk3BtbrWB5mELrmly9MbK2PDXgYka14M9fW7J8xskQ2RLtCFWGcPLmqnN0IeE8RDc8MV4Jc3vhqejuGV11s7-Xe4R9rDIDQtazlyLhK2ct-MF235VslqhBwC5nXeKwieYVh0DOEi6oM60RftsouXysJ0GR85rwvNu3VyPMDRMZgOeJtPuyslmXCNEBHD3v6tNUT04-IWKTu8rd_UIO8rjq8T8O5dKgIArlItaWkld3xp_Slkn1ruLrykeq38hNRBkoYDGlJ03JOa4abaI8fL47ko-NqvefjM79P30mpOdRKEvLskF9vQ_erOEvyPqV2ktDmPfFxdTU4Cp3c_QZGqJRDehQ6cKev2wo0ujf0q4Z-pij85W8t6foi96dYO4VFuXFrcZGCkkmNdG3VoJG_EFug5G-cupRVbRbJCZYxde7c6CpJngIUb6isBDvQ18zGZPeYOwdORHIv7h7CwO85m3fiKxir2tlgRARrojUEph29wOe5pHhkwWLg2RBGk9P3MBRFDRoWPn76eHuw9mWLi1_NPB9xnMxuu3oEmKr_jDOu4KbJg91EwRvk1UdChGdZ_e-eZGGyYnughgJsPUoxMizEl48kPeRNRCl4bDwifJRoFs0_yGleaMP4sH5WB31ecjQEtWHIT0uLDs9TFDPlXS_dFeaRgmzsDkMPOYD8KtPrYXU5Hvxcuv9wRqtKPHt03w8FpjHMy4Lj-w_ijsR21PjnlBHzElIbFx0-sM7mLWhPCY-dT09-pSBcRZ2Gaxp4OsPeGK0CRBXiKUAV7JXiRrlc354CJr6noINO31FAhSXk0brSHrPEx8rx6UcmDorkpmIAnTNYUaLK6KjsKk8PXkHSuuISgv2oBXWo-LzFu3uNsF6i0gARggRUaopKlxyPLOki6fsLIUSY1IDEVJ46mHG5wa_k3D2k562bSzFN6qev2mUwaLvBlrsf3IaItJHF8zKOu_cX3-VCDrKqhk4GzhCUiagvOmYuSOtwc9bjNhTdOkx7jHwmaV_o71KHbl74t86Cy3fnveNjgn8fgysqhfZcCG0wmVtMHA4TzA2Xgxpg3BCev-yc-d_1f7-2CQAZI0DvtBxIIusL4vFZgB-QYCC6WQh9gL2VpaAu8z-jP6cTNOuiv-66MFg2LXZpBBr4nWPLXY0BM5Cly4xDYZjDjtsTDag5QmViVxhABOhnaZuO3aPxB4rDNGTaP26i50GAbxwPGSwFPjXImhnd7Rku1n9vpTNMfLeZt5nmNDCPP7-ACWFsllV0WE4hs5r50f1NFrICrMRz8HjiwBXiTDT0w9Ia3yTBRCrLPj9E3gNSRDlHJNp-zT5dnZk63VCYtULw2vL6I8yKjw37JgMyNPz3nTSQbjX80M61WsQHkg";
+const ACCESS_TOKEN =
+  "sl.u.AF2g7z5iVuB42KegiI6AMTYWqXNMr4inldFv3zLuohOnRO-p12SCJ64oEECdW1ihmherjO9POTTAeB5epx4pJOqVaXosKjIZMmO1kpX3vsCOvKC1blV2zwtJ7YzDIJ4Cg735QCk6Vz_Bhyyd_bDuyrUSfnI4QPWmAzb8VvWVrXAudeTyKi7ar4tBKwDmN_bWrYtqQo63l15N6Z7r9tUrIf-Lk_KPQXKRtEqc725yTbMcE3XvxqELZ8vuV3oy9GQMyyKcA-PrYb--BC1MkwOKqSbRRSqNuthT3leI77VIUu_q1Q8OJ6KAnTkp5eakL_43PVDEhM4HsMeQihzZbnBzMGdAe_dHrzob8spIe8u2X2-YKJypGretKrz3dAqFy39zqRRxqg3mKl2M0FVW5DuMVjkS_XZOo6PUd3mvjp52NEOwkuT6SGAfOeM_mWqcoAO4y8-YDvExkXbkA7DCdR3Vu16HuEjtv7K1yXQyzvAjcz-proQmhRAR26pXJ0mFrq7Mq81GNsfUfi3DgI-WnmfaWKNOf4G6BNM6lYUiU-vcUA_MUgHsLLgp7lv2fror20nD-vohG31jiFGMAoGcH_5lwWaXBa2lkv9WZC2M6OrxAjpucbjIjRxzqwbXawfpIGZlNu0mFbDw8wZI9GJvtV1EMNsV8b-jWlt-U6MTwYwoPu4OOA6cMgYdfNtfPTL3mYZdR9mQPwfZjb8dN_wwK_NkUY79e1tgKrKJDn77qN6avVe-8HFtleQ05043e_lit20vOEbeY6USxiVBQwYLyPZucoD4lSLy22QE6yV6Dq1LGidEEXFODYd8q0fnDNLXTx8CkEfUlkWUbJ4K4QE7mN3bJcce0uVdeIBd5BFL4OFlRo1xW6MONv1UTTRMAMEzDUdmbszSfZMqg2AEDFFMn30WuQmMYSfH2l05BuD4X9nvO5qW8p4r-G2nJ7q9KVVLy82_SpI4JeN5wSAj7M7mvsvPfi_KYEGzhUHlFANHcxP7JtOfjwbcF3SE758LPZT97rsvffOfOPPLak4rXuZuGje-JC-prA4Eekm8fRuVJktHAAeXbdGGudUxrejZVJAVY-6XZ3K_w1C1gD7cZt_pQwQPj0__BbBz1xftpLt6Vc9Ln5xzFvB8XWjlQNzC1Z6c6wuJCvK-nj9YkTLkuR7tD7604KojaFRTPF3XVHnAcYdJKoLK-sV5Wa-O-l0yfMG6U5DuB6_Cc_puHsB8VOAIGSv26HQv2WUv6hq1mlIPkupSq9-siuhQTsCpuCHt4CkMqIpmOSGOcAu7tGgXdmW-rFvy392bsa_7UNZCKQu7Bn_CmSjQ5E3zFhxGHJwPepdMVk6jp8R26WekOf8iy6NCOJUyxY5DZIMh3pIOvPVWBDrQ6O188EVi1H_GOqACQ5KCAm8nVheR4EyQg6jeQhZwRJT3ghFfGnjPcrjGWr7W3rQGh_j0LQ";
 const FOLDER_PATH = "/Pamiątka";
 
 let cursors = [];
+let allFiles = [];
+const FILE_LIMIT = 50;
 loadFiles();
 async function loadFiles(page = 1) {
   const fileListDiv = document.getElementById("file-list");
@@ -23,7 +25,7 @@ async function loadFiles(page = 1) {
         body: JSON.stringify({
           path: FOLDER_PATH,
           recursive: false,
-          limit: 1,
+          limit: FILE_LIMIT,
         }),
       }
     );
@@ -39,11 +41,6 @@ async function loadFiles(page = 1) {
     }
 
     files = listData.entries.filter((e) => e[".tag"] === "file");
-    if (page === 1) {
-      allFiles = files;
-    } else {
-      allFiles = allFiles.concat(files);
-    }
     if (listData.has_more) cursors[1] = listData.cursor;
   } else {
     cursor = cursors[page - 1];
@@ -78,6 +75,7 @@ async function loadFiles(page = 1) {
     files = listData.entries.filter((e) => e[".tag"] === "file");
     if (listData.has_more) cursors[page] = listData.cursor;
   }
+  allFiles = files;
 
   if (files.length === 0) {
     fileListDiv.innerHTML =
@@ -117,7 +115,7 @@ async function loadFiles(page = 1) {
     col.innerHTML = `
       <div class="thumb-container">
         <img src="${url}" alt="Thumbnail" onclick="openPreview(${
-      (page - 1) * 50 + index
+      (page - 1) * FILE_LIMIT + index
     })">
       </div>
     `;
@@ -129,14 +127,14 @@ async function loadFiles(page = 1) {
     paginationControls.innerHTML = `
   ${
     page > 1
-      ? `<button  type="button" class="btn btn-link p-0 m-0 text-decoration-none navigation-button" onclick="loadFiles(${
+      ? `<button  type="button" class="btn p-0 m-0 text-decoration-none navigation-button" onclick="loadFiles(${
           page - 1
         })">←</button>`
       : ""
   }
   ${
     cursors[page]
-      ? `<button type="button" class="btn btn-link p-0 m-0 text-decoration-none navigation-button" onclick="loadFiles(${
+      ? `<button type="button" class="btn p-0 m-0 text-decoration-none navigation-button" onclick="loadFiles(${
           page + 1
         })">→</button>`
       : ""
@@ -159,7 +157,6 @@ function b64ToBlob(b64Data, contentType = "", sliceSize = 512) {
 }
 
 let currentPreviewIndex = 0;
-let allFiles = [];
 
 function openPreview(index) {
   currentPreviewIndex = index;
@@ -211,7 +208,6 @@ function toASCIIHeader(obj) {
   });
 }
 
-// Touch/swipe navigation
 let touchStartX = 0;
 let touchEndX = 0;
 const previewModal = document.getElementById("preview-modal");
@@ -227,7 +223,7 @@ previewModal.addEventListener("touchend", (e) => {
 
 function handleSwipe() {
   const deltaX = touchEndX - touchStartX;
-  if (Math.abs(deltaX) < 50) return; // ignore small swipes
+  if (Math.abs(deltaX) < 50) return;
 
   if (deltaX < 0 && currentPreviewIndex < allFiles.length - 1) {
     currentPreviewIndex++;
