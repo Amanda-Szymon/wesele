f824758e5f368faa1cdb7acac08175cb59620ecd
Amanda-Szymon <amandaszymonwedding@gmail.com>
2025-08-12 08:25:51 +0200

    Update script.js
    file limit 30

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const FOLDER_PATH = "/Pamiątka";
 
 let cursors = [];
 let allFiles = [];
-const FILE_LIMIT = 50;
+const FILE_LIMIT = 30;
 loadFiles();
 async function loadFiles(page = 1) {
   const fileListDiv = document.getElementById("file-list");
